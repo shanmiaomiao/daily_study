65e632ef2b8b964834b70160309514d6cd69add4
shanmiaomiao <shanguangyan@gmail.com>
2016-10-15 00:04:25 +0800

    测试int类型转String类型的三种方法

---
+++ Test2.java
@@ -0,0 +1,36 @@
+//
+
+public class Test2 {
+	public static void main(String[] args){
+	  String  m = "";
+		m = print(10,20);
+		System.out.println(m);
+	}
+	 
+public static String print(int a,int b ){
+	int number = a + b;
+	String c = "";
+	System.out.println(" a++ =" + (a++));
+	System.out.println("a+a="+(a+a));
+	System.out.println(" a + b =" + (a + b));
+	System.out.println(" a - b =" + (a - b));
+	System.out.println(" a * b =" + (a * b));
+	System.out.println(" a / b =" + (a / b));
+	System.out.println("a%b="+(a%b));
+	//System.outprintln("a % b =" + (a % b));i
+	System.out.println(" a++ =" + (a++));
+	System.out.println(" a-- =" + (a--));
+	System.out.println(" --a =" + (--a));
+	System.out.println(" ++a =" + (++a));
+	//return String.valueOf(number);
+	//return Integer.toString(number);
+	 return number+"";
+
+	// 查看 d++ 与 ++d  的不同
+	
+	//System.out.println(" b++ =" + ( b++ ));
+	//System.out.println(" ++b =" + ( ++b ));
+	
+	}
+
+}
